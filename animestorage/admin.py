67f1e6f5c6ac8ee35813142568cba947ae6b0c41
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-25 06:36:13 +0600

    registered models in admin

--- animestorage/admin.py
+++ animestorage/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from animestorage.models import Studio, Genre, Status, AnimeType, AgeRating, Anime
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    readonly_fields = ['id', 'date_added']
+
+
+@admin.register(Studio)
+class StudioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    readonly_fields = ['id', 'date_added']
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    readonly_fields = ['id', 'date_added']
+
+
+@admin.register(AnimeType)
+class AnimeTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    readonly_fields = ['id', 'date_added']
+
+
+@admin.register(AgeRating)
+class AgeRatingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    readonly_fields = ['id', 'date_added']
+
+
+@admin.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'poster', 'title', 'qty_episodes', 'len_episode']
+    readonly_fields = ['id', 'date_added']
