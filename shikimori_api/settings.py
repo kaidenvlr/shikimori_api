8737e6f37dcbe3fd20993fa9d9714f380dc033c0
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-25 06:35:46 +0600

    apps in settings

--- shikimori_api/settings.py
+++ shikimori_api/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # drf
+    'rest_framework',
+    # my apps
+    'animestorage',
 ]
 
 MIDDLEWARE = [
