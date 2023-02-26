6fd8069c5060a9e496c1cf5f10148be1ab831cdf
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-26 08:39:17 +0600

    serializers for all models

---
+++ animestorage/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from animestorage.models import Genre, Studio, Status, AnimeType, Anime
+
+
+class GenreSerializer(serializers.Serializer):
+    class Meta:
+        model = Genre
+        fields = ['id', 'title', 'description', 'date_added']
+
+
+class StudioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Studio
+        fields = ['id', 'title', 'date_added']
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = ['id', 'title', 'date_added']
+
+
+class AnimeTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnimeType
+        fields = ['id', 'title', 'date_added']
+
+
+class AnimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Anime
+        fields = ['id', 'title', 'description', 'qty_episodes',
+                  'len_episode', 'studio', 'type', 'genre',
+                  'poster', 'status', 'release_year', 'date_added']
