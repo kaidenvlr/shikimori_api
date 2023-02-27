a229f3b13947a7d657a58685ea42fd07c174d715
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-27 07:00:42 +0600

    urls of anime type

--- shikimori_api/urls.py
+++ shikimori_api/urls.py
@@ -4,8 +4,18 @@ from django.urls import path, include
 from animestorage import views
 
 urlpatterns = [
+    # admin
     path('admin/', admin.site.urls),
+
+    # animestorage
+    # auth
     path('api/v1/auth/', include('rest_framework.urls')),
+    # anime
     path('api/v1/anime/', views.AnimeAPIList.as_view()),
     path('api/v1/anime/<int:pk>/', views.AnimeAPIRetrieveUpdateDestroy.as_view()),
+    # anime type
+    path('api/v1/animetype/', views.AnimeTypeAPIList.as_view()),
+    path('api/v1/animetype/<int:pk>/', views.AnimeTypeAPIRetrieveUpdateDestroy.as_view()),
+
+    # userstorage
 ]
