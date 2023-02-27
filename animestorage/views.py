ce267d102c6f90fb1c7dcba4ff1e2970d4587e8b
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-27 07:00:53 +0600

    views of anime type

--- animestorage/views.py
+++ animestorage/views.py
@@ -5,12 +5,27 @@ from animestorage import serializers
 from animestorage.permissions import IsAdminOrReadOnly
 
 
+# anime
 class AnimeAPIList(generics.ListCreateAPIView):
     queryset = models.Anime.objects.all()
     serializer_class = serializers.AnimeSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class AnimeAPIRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Anime.objects.all()
     serializer_class = serializers.AnimeSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+# anime type
+class AnimeTypeAPIList(generics.ListCreateAPIView):
+    queryset = models.AnimeType.objects.all()
+    serializer_class = serializers.AnimeTypeSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class AnimeTypeAPIRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.AnimeType.objects.all()
+    serializer_class = serializers.AnimeTypeSerializer
     permission_classes = (IsAdminOrReadOnly,)
