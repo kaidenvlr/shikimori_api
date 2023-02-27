f673fe5c9238625a80689cf68aba72afc042f156
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-27 07:05:52 +0600

    cleanup

--- shikimori_api/urls.py
+++ shikimori_api/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
 
-    # animestorage
     # auth
     path('api/v1/auth/', include('rest_framework.urls')),
+
+    # animestorage
+
     # anime
     path('api/v1/anime/', views.AnimeAPIList.as_view()),
     path('api/v1/anime/<int:pk>/', views.AnimeAPIRetrieveUpdateDestroy.as_view()),
@@ -18,4 +20,5 @@ urlpatterns = [
     path('api/v1/animetype/<int:pk>/', views.AnimeTypeAPIRetrieveUpdateDestroy.as_view()),
 
     # userstorage
+    
 ]
