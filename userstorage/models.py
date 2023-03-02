ec72f107d335fca6a377e5fa6f1a078885fbb7a4
kaidenvlr <jzxcvblessed@gmail.com>
2023-03-03 03:43:50 +0600

    profile anime list model

--- userstorage/models.py
+++ userstorage/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from animestorage.models import Anime
 
 
 class AnimeWatchStatus(models.Model):
@@ -10,3 +12,17 @@ class AnimeWatchStatus(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProfileListAnime(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    status = models.ForeignKey(AnimeWatchStatus, on_delete=models.CASCADE, verbose_name='Статус')
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE, verbose_name='Аниме')
+    qty_episodes = models.PositiveSmallIntegerField(verbose_name='Количество просмотренных эпизодов')
+
+    class Meta:
+        verbose_name = 'Аниме у пользователя'
+        verbose_name_plural = 'Аниме у пользователей'
+
+    def __str__(self):
+        return f"{self.user.username} - {self.anime.title}"
